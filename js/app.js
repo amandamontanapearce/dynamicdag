8da5690ed9de1db05765d0d71e0413fe5de9d200
AmandaMontana Pearce <amandamontanapearce@gmail.com>
2016-09-11 11:52:23 -0600

    add non-working d3 modal

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+   $('.modal-trigger').leanModal();
+   $('select').material_select();
+   $('.nodeLinkModal').click( function(){
+     $('.modal').css('display', 'block')
+   });
+ });
